d907eac96f7ec1ec37edab023175dc4f29d60771
Apri Pandu Wicaksono <sarahpalastrin@gmail.com>
2023-09-06 14:32:17 +0700

    chore: function course

--- function.lua
+++ function.lua
@@ -1,3 +1,6 @@
+--[[
+  In lua if you want to make a function, you can use the following syntax:
+]]
 function square(x)
   return x * x
 end
@@ -18,3 +21,13 @@ print(square(5))
 print(getSmallerValue(5, 10))
 print(convertMilesToFeet(2))
 print(getAreaOfRightTriangle(5))
+
+--[[
+  But if you make a function without local keyword, it will be a global function.
+  So, if you want to make a local function, you can use the following syntax:
+]]
+local function getHalfOfNumber(number)
+  return number / 2
+end
+
+print(getHalfOfNumber(10))
